c64592fda07395e53834587677e2258d2908263e
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-02-26 13:56:36 -0600

    Add files via upload

---
+++ desafio1/src/app/componentes/Clase.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ClaseData = () =>{
+    return(
+        <h3> Clase </h3>
+        /*
+        *(yoga, spinning, pesas, etc.)
+        */ 
+    )
+}
---
+++ desafio1/src/app/page.js
@@ -0,0 +1,19 @@
+import styles from "./page.module.css";
+import ListaClases from "./componentes/ListaClases";
+import ReservaClase from "./componentes/ReservaClase";
+ 
+export default function Home() {
+  return (
+    <main className={styles.main}>
+    <div className="App">
+      <div>
+      <p>s      </p>
+      <ListaClases></ListaClases>
+      </div>
+    </div>
+    
+    </main>
+
+    
+  );
+}
